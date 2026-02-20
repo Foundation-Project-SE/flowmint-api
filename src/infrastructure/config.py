5b480f91fb75d9bab99078674f3253796f5549ff
Juan Facundo Bazan <33848453+FacuBzn@users.noreply.github.com>
2026-02-19 23:05:36 -0300

    Develop (#4)
    * feat: configure CORS and environment validation for Next.js integration
    
    * feat: setup asyng sqlalchemy, configure alembic and connect to supabase pooler
    
    * chore: update database configuration and add SQL echo setting
    
    ---------
    
    Co-authored-by: NeilC <neilccu@gmail.com>

--- src/infrastructure/config.py
+++ src/infrastructure/config.py
@@ -1,13 +1,24 @@
-import os
+from typing import List
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import PostgresDsn, field_validator, AnyHttpUrl
+
 
 class Settings(BaseSettings):
-    # Required variables (without default value)
-    DATABASE_URL: str
-    
-    # Default variables values
     PROJECT_NAME: str = "FlowMind API"
     API_V1_STR: str = "/api/v1"
+    DATABASE_URL: PostgresDsn
+    SQL_ECHO: bool = False  # Set to True for SQL debug output; keep False in production
+
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    
+    @field_validator("BACKEND_CORS_ORIGINS", mode="before")
+    @classmethod
+    def assemble_cors_origins(cls, v: str | List[str]) -> List[str] | str:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
     
     model_config = SettingsConfigDict(
         env_file=".env", 
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 from src.infrastructure.config import settings
 from src.infrastructure.api.v1.router import api_router
 
@@ -10,6 +11,17 @@ app = FastAPI(
     description="Backend for FlowMind using Hexagonal Architecture",
     version="0.1.0",
 )
+
+# --- CORS configuration ---
+# if origins list has elements, middleware gets active
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"], # Allow all methods (GET, POST, PUT, DELETE)
+        allow_headers=["*"], # Allow all headers (Authorization, etc.)
+    )
 
 # Main router v1 include
 app.include_router(api_router, prefix=settings.API_V1_STR)
